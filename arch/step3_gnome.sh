4908ee7dd6a2f73a85ac08fb0bfbc03f2b835a5e
edergilson <edergilson@gmail.com>
2018-04-05 21:34:36 -0300

    Update step3_gnome.sh

--- arch/step3_gnome.sh
+++ arch/step3_gnome.sh
@@ -3,7 +3,7 @@
 sudo ip link set $1 up
 sudo dhcpcd $1
 
-sudo pacman -Syu gnome gnome-extra ttf-dejavu ttf-hack ttf-liberation ttf-ms-fonts cantarell-fonts noto-fonts gdm xorg xorg-xinit
+sudo pacman -Syu gnome gnome-extra ttf-dejavu ttf-hack ttf-liberation ttf-ms-fonts cantarell-fonts noto-fonts gdm xorg xorg-xinit networkmanager p7zip unrar tar rsync linux-headers
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 sed -i '/exec /s/^/# /g' ~/.xinitrc
 echo "exec gnome-session" >> ~/.xinitrc
