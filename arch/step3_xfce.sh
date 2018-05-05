5cb9bb2a3bdc3d830e5eaf853b894d7819f330dd
edergilson <edergilson@gmail.com>
2018-05-05 14:58:56 -0300

    Create step3_xfce.sh

---
+++ arch/step3_xfce.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo ip link set $1 up
+sudo dhcpcd $1
+
+sudo pacman -Sy xorg xorg-xinit
+sudo pacman -Sy xfce4 xfce4-goodies netowrkmanager ttf-dejavu ttf-hack ttf-liberation cantarell-fonts noto-fonts lightdm lightdm-gtk-greeter
+sudo pacman -Sy xfce4-notifyd network-manager-applet udisks2 thunar-volman gvfs p7zip unrar tar rsync linux-headers
+sudo systemctl enable NetworkManager.service
+sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
+sed -i '/exec /s/^/# /g' ~/.xinitrc
+echo "exec startxfce4" >> ~/.xinitrc
+sudo chmod +x ~/.xinitrc
+sudo systemctl enable NetworkManager
+sudo reboot
