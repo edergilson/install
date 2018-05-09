02ca4cb08e28847c2cb5d019df1d8007808e437a
edergilson <edergilson@gmail.com>
2018-05-09 19:53:09 -0300

    Update step3_xfce.sh

--- arch/step3_xfce.sh
+++ arch/step3_xfce.sh
@@ -4,7 +4,7 @@ sudo ip link set $1 up
 sudo dhcpcd $1
 
 sudo pacman -Sy xorg xorg-xinit
-sudo pacman -Sy xfce4 xfce4-goodies netowrkmanager ttf-dejavu ttf-hack ttf-liberation cantarell-fonts noto-fonts lightdm lightdm-gtk-greeter
+sudo pacman -Sy xfce4 xfce4-goodies networkmanager ttf-dejavu ttf-hack ttf-liberation cantarell-fonts noto-fonts lightdm lightdm-gtk-greeter
 sudo pacman -Sy xfce4-notifyd network-manager-applet udisks2 thunar-volman gvfs p7zip unrar tar rsync linux-headers
 sudo systemctl enable NetworkManager.service
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
