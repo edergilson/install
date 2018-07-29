cfa382917e5a812c28d47daf74aa813976fd9074
edergilson <edergilson@gmail.com>
2018-07-28 21:29:55 -0300

    Update step3_kde.sh

--- arch/step3_kde.sh
+++ arch/step3_kde.sh
@@ -4,7 +4,7 @@ sudo ip link set $1 up
 sudo dhcpcd $1
 
 ####### INSTALL KDE!!! #######
-sudo pacman -Sy plasma-meta sddm xorg xorg-xinit ttf-dejavu ttf-liberation networkmanager
+sudo pacman -Sy plasma-meta sddm xorg xorg-xinit ttf-dejavu ttf-liberation xf86-input-mouse xf86-input-evdev networkmanager
 sudo pacman -Sy konsole okular ark kate gwenview dolphin p7zip unrar tar rsync linux-headers
 # sudo nano /etc/X11/xinit/xinitrc ###Comment all exec and add exec startkde
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
