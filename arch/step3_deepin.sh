2d63359a6dc5496bf866c7a9c08ed00dfb1913ed
edergilson <edergilson@gmail.com>
2018-07-28 21:28:53 -0300

    Update step3_deepin.sh

--- arch/step3_deepin.sh
+++ arch/step3_deepin.sh
@@ -3,7 +3,7 @@
 sudo ip link set $1 up
 sudo dhcpcd $1
 
-sudo pacman -Syu deepin deepin-extra ttf-dejavu ttf-hack ttf-liberation cantarell-fonts noto-fonts xorg xorg-xinit networkmanager lightdm
+sudo pacman -Syu deepin deepin-extra ttf-dejavu ttf-hack ttf-liberation cantarell-fonts noto-fonts xorg xorg-xinit xf86-input-mouse xf86-input-evdev networkmanager lightdm
 sudo pacman -Syu p7zip unrar tar rsync linux-headers
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 sed -i '/exec /s/^/# /g' ~/.xinitrc
