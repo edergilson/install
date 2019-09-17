20d790b4595efcc5adffca20af8f1b108e55be94
edergilson <edergilson@gmail.com>
2019-09-16 21:21:10 -0300

    Update step4_deepin.sh

--- arch/step4_deepin.sh
+++ arch/step4_deepin.sh
@@ -2,42 +2,46 @@ echo "
 [multilib]
 Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
 
-echo "
-[archlinuxfr]
-SigLevel = Never
-Server = http://repo.archlinux.fr/\$arch" | sudo tee -a /etc/pacman.conf
+sudo pacman -Syu
 
-sudo pacman -Sy nvidia nvidia-utils mesa xf86-video-intel bumblebee lib32-virtualgl lib32-nvidia-utils
+git clone https://aur.archlinux.org/yay.git ~/yay && cd ~/yay && makepkg -si && sudo rm -rf ~/yay && yay --editmenu --nodiffmenu --save
+
+sudo sed -i '/#UseSyslog/s/#//g' /etc/pacman.conf
+sudo sed -i '/#Color/s/#//g' /etc/pacman.conf
+sudo sed -i '/#TotalDownload/s/#//g' /etc/pacman.conf
+sudo sed -i '/#VerbosePkgLists/s/#//g' /etc/pacman.conf
+
+sudo pacman -Syu && yay -Syua
+
+sudo pacman -S nvidia nvidia-utils mesa xf86-video-intel bumblebee lib32-virtualgl lib32-nvidia-utils
 
 sudo usermod -aG bumblebee eder && sudo systemctl enable bumblebeed.service
 
-pacman -Sy yaourt && yaourt -Sy
-
-// INSTALL BROWSERS //
+## INSTALL BROWSERS ##
 sudo pacman -S firefox chromium opera
 
-PACKAGE="qbittorrent pidgin pavucontrol pulseaudio"
+PACKAGE="qbittorrent pavucontrol pulseaudio"
 
 gpg --recv-keys --keyserver hkp://18.9.60.141 5CC908FDB71E12C2
 
-sudo pacman -Sy exfat-utils fuse-exfat a52dec faac faad2 flac jasper lame libdca libdv gst-libav libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore flashplugin libdvdcss libdvdread libdvdnav dvd+rw-tools dvdauthor dvgrab
+sudo pacman -S exfat-utils fuse-exfat a52dec faac faad2 flac jasper lame libdca libdv gst-libav libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore flashplugin libdvdcss libdvdread libdvdnav dvd+rw-tools dvdauthor dvgrab pepper-flash
 
-sudo pacman -S zsh vlc clementine virtualbox thunderbird youtube-dl audacity meld htop docker docker-compose atom mysql-workbench steam git xdotool pulseaudio-bluetooth tlp tlp-rdw $PACKAGE
+sudo pacman -S zsh vlc clementine virtualbox youtube-dl audacity meld htop docker docker-compose atom mysql-workbench steam git xdotool pulseaudio-bluetooth tlp tlp-rdw $PACKAGE
 
 sudo usermod -aG docker $USER && sudo usermod -aG vboxusers $USER && sudo modprobe vboxdrv && sudo systemctl enable tlp && sudo systemctl start tlp && sudo systemctl enable docker
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-yaourt -S google-chrome 
-yaourt -S vivaldi 
-yaourt -S wps-office 
-yaourt -S spotify 
-yaourt -S neofetch-git 
-yaourt -S sublime-text-dev 
-yaourt -S visual-studio-code-bin 
-yaourt -S gitkraken 
-yaourt -S gargoyle-git 
-yaourt -S rambox 
-yaourt -S smartgit 
-yaourt -S postman 
-yaourt -S sweethome3d
+yay -S google-chrome 
+yay -S opera-ffmpeg-codecs
+yay -S vivaldi 
+yay -S wps-office ttf-wps-fonts wps-office-extension-portuguese-brazilian-dictionary 
+yay -S spotify 
+yay -S neofetch-git 
+yay -S sublime-text-dev 
+yay -S visual-studio-code-bin 
+yay -S gitkraken 
+yay -S gargoyle-git 
+yay -S smartgit 
+yay -S insomnia 
+yay -S sweethome3d
