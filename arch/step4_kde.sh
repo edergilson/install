8256f8e0f7de8d868137abe80d6d7d866e522c01
edergilson <edergilson@gmail.com>
2019-10-08 20:48:54 -0300

    Update step4_kde.sh

--- arch/step4_kde.sh
+++ arch/step4_kde.sh
@@ -2,7 +2,14 @@ echo "
 [multilib]
 Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
 
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ../ && sudo rm -rf yay && yay --editmenu --nodiffmenu --save
+sudo pacman -Syu
+
+git clone https://aur.archlinux.org/yay.git ~/yay && cd ~/yay && makepkg -si && sudo rm -rf ~/yay && yay --editmenu --nodiffmenu --save
+
+sudo sed -i '/#UseSyslog/s/#//g' /etc/pacman.conf
+sudo sed -i '/#Color/s/#//g' /etc/pacman.conf
+sudo sed -i '/#TotalDownload/s/#//g' /etc/pacman.conf
+sudo sed -i '/#VerbosePkgLists/s/#//g' /etc/pacman.conf
 
 sudo pacman -Syu && yay -Syua
 
@@ -13,21 +20,21 @@ sudo usermod -aG bumblebee eder && sudo systemctl enable bumblebeed.service
 ## INSTALL BROWSERS ##
 sudo pacman -S firefox chromium opera
 
-PACKAGE="kcalc ktorrent yakuake kget kdeconnect kompare ksysguard korganizer kmail"
+PACKAGE="kcalc ktorrent yakuake kget kdeconnect kompare ksysguard korganizer kmail spectacle"
 
 gpg --recv-keys --keyserver hkp://18.9.60.141 5CC908FDB71E12C2
 
 sudo pacman -S exfat-utils fuse-exfat a52dec faac faad2 flac jasper lame libdca libdv gst-libav libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore flashplugin libdvdcss libdvdread libdvdnav dvd+rw-tools dvdauthor dvgrab
 
-sudo pacman -S zsh vlc clementine virtualbox thunderbird youtube-dl audacity meld htop docker docker-compose atom mysql-workbench steam git xdotool pulseaudio-bluetooth tlp tlp-rdw $PACKAGE
+sudo pacman -S zsh vlc clementine virtualbox youtube-dl audacity meld htop docker docker-compose atom mysql-workbench steam git xdotool pulseaudio-bluetooth tlp tlp-rdw $PACKAGE
 
-sudo usermod -aG docker $USER && sudo usermod -aG vboxusers $USER && sudo modprobe vboxdrv && sudo systemctl enable tlp && sudo systemctl start tlp && sudo systemctl enable docker
+sudo usermod -aG docker $USER && sudo usermod -aG vboxusers $USER && sudo modprobe vboxdrv && sudo systemctl enable tlp && sudo systemctl start tlp && sudo systemctl enable docker && sudo systemctl enable bluetooth
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 yay -S google-chrome 
-yay -S  opera-ffmpeg-codecs 
-yay -S vivaldi vivaldi-snapshot-ffmpeg-codecs 
+yay -S opera-ffmpeg-codecs 
+yay -S vivaldi 
 yay -S wps-office ttf-wps-fonts wps-office-extension-portuguese-brazilian-dictionary 
 yay -S spotify 
 yay -S neofetch-git 
