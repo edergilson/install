bc6973e7c3ffed842efbdb3322c95b2e48aa6195
edergilson <edergilson@gmail.com>
2020-03-29 15:39:24 -0300

    Update step3_gnome.sh

--- arch/step3_gnome.sh
+++ arch/step3_gnome.sh
@@ -3,7 +3,7 @@
 sudo ip link set $1 up
 sudo dhcpcd $1
 
-sudo pacman -Syu gnome gnome-extra ttf-dejavu ttf-hack ttf-liberation cantarell-fonts noto-fonts gdm xorg xorg-xinit xf86-input-mouse xf86-input-evdev networkmanager p7zip unrar tar rsync linux-headers
+sudo pacman -Syu gnome gnome-extra ttf-dejavu ttf-hack ttf-liberation cantarell-fonts noto-fonts gdm xorg xorg-xinit libinput xf86-input-evdev networkmanager p7zip unrar tar rsync linux-headers
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 sed -i '/exec /s/^/# /g' ~/.xinitrc
 echo "exec gnome-session" >> ~/.xinitrc
