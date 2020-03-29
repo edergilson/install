2009471b71bd6ee2aef7f1f3228c09801b783581
edergilson <edergilson@gmail.com>
2020-03-29 16:35:00 -0300

    Update step4_kde.sh

--- arch/step4_kde.sh
+++ arch/step4_kde.sh
@@ -20,11 +20,11 @@ sudo usermod -aG bumblebee eder && sudo systemctl enable bumblebeed.service
 ## INSTALL BROWSERS ##
 sudo pacman -S firefox chromium opera
 
-PACKAGE="kcalc ktorrent yakuake kget kdeconnect kompare ksysguard korganizer kmail spectacle elisa falkon kdevelop kdevelop-php"
+PACKAGE="kcalc ktorrent yakuake kget kdeconnect kompare ksysguard korganizer kmail spectacle elisa falkon kdevelop kdevelop-php ffmpegthumbnailer ffmpegthumbs"
 
 gpg --recv-keys --keyserver hkp://18.9.60.141 5CC908FDB71E12C2
 
-sudo pacman -S exfat-utils fuse-exfat a52dec faac faad2 flac jasper lame libdca libdv gst-libav libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore flashplugin libdvdcss libdvdread libdvdnav dvd+rw-tools dvdauthor dvgrab
+sudo pacman -S exfat-utils fuse-exfat a52dec faac faad2 flac jasper lame libdca libdv gst-libav libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore flashplugin libdvdcss libdvdread libdvdnav dvd+rw-tools dvdauthor dvgrab cups pepper-flash
 
 sudo pacman -S zsh vlc clementine virtualbox youtube-dl audacity meld htop docker docker-compose atom mysql-workbench steam git xdotool pulseaudio-bluetooth tlp tlp-rdw $PACKAGE
 
@@ -33,4 +33,5 @@ sudo usermod -aG docker $USER && sudo usermod -aG vboxusers $USER && sudo modpro
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 yay -S google-chrome opera-ffmpeg-codecs vivaldi libreoffice-fresh spotify neofetch-git sublime-text-dev visual-studio-code-bin gitkraken gargoyle-git rambox-bin smartgit insomnia sweethome3d sweethome3d-furniture-library brave-bin 
+
 yay -S latte-dock-git libdbusmenu-gtk3 lib32-libdbusmenu-gtk3 appmenu-gtk-module-git breeze-enhanced-git openvpn networkmanager-openvpn plasma5-applets-window-title-git plasma5-applets-window-appmenu-git plasma5-applets-window-buttons-git kwin-effect-shapecorners-git libinput-gestures kwin-scripts-parachute plasma5-applets-eventcalendar
