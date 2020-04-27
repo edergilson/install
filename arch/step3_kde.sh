9981f5a2c044025f8985b5309fe63cde4c303085
edergilson <edergilson@gmail.com>
2020-04-27 08:28:49 -0300

    Update step3_kde.sh

--- arch/step3_kde.sh
+++ arch/step3_kde.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-sudo ip link set $1 up
-sudo dhcpcd $1
+# sudo ip link set $1 up
+# sudo dhcpcd $1
 
 ####### INSTALL KDE!!! #######
+echo "
+[multilib]
+Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
+
+sudo pacman -Sy nvidia nvidia-utils mesa xf86-video-intel bumblebee lib32-virtualgl lib32-nvidia-utils
+sudo usermod -aG bumblebee eder && sudo systemctl enable bumblebeed.service
+
 sudo pacman -Sy plasma-meta sddm xorg xorg-xinit ttf-dejavu ttf-liberation libinput xf86-input-evdev networkmanager
 sudo pacman -Sy konsole okular ark kate gwenview dolphin p7zip unrar tar rsync linux-headers
 # sudo nano /etc/X11/xinit/xinitrc ###Comment all exec and add exec startkde
