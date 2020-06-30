6f5d5f549a68bd2b1243fcaa8eafc8aec6544bab
edergilson <edergilson@gmail.com>
2020-06-30 15:41:42 -0300

    Update step3_gnome.sh

--- arch/step3_gnome.sh
+++ arch/step3_gnome.sh
@@ -3,6 +3,9 @@
 sudo ip link set $1 up
 sudo dhcpcd $1
 
+sudo pacman -Sy nvidia nvidia-utils mesa bumblebee nvidia-settings lib32-virtualgl lib32-nvidia-utils
+sudo usermod -aG bumblebee eder && sudo systemctl enable bumblebeed.service
+
 sudo pacman -Syu gnome gnome-extra ttf-dejavu ttf-hack ttf-liberation cantarell-fonts noto-fonts gdm xorg xorg-xinit libinput xf86-input-evdev networkmanager p7zip unrar tar rsync linux-headers
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 sed -i '/exec /s/^/# /g' ~/.xinitrc
