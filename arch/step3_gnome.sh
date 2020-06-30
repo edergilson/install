9e175a3cbdecbbe416ca423d8bec4626a69ef8bb
edergilson <edergilson@gmail.com>
2020-06-30 15:39:39 -0300

    Update step3_gnome.sh

--- arch/step3_gnome.sh
+++ arch/step3_gnome.sh
@@ -6,7 +6,9 @@ sudo dhcpcd $1
 sudo pacman -Syu gnome gnome-extra ttf-dejavu ttf-hack ttf-liberation cantarell-fonts noto-fonts gdm xorg xorg-xinit libinput xf86-input-evdev networkmanager p7zip unrar tar rsync linux-headers
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 sed -i '/exec /s/^/# /g' ~/.xinitrc
-echo "exec gnome-session" >> ~/.xinitrc
+echo "export XDG_SESSION_TYPE=x11
+export GDK_BACKEND=x11
+exec gnome-session" >> ~/.xinitrc
 sudo systemctl enable NetworkManager
 sudo systemctl enable gdm
 
