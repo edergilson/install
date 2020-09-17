3676b62864401135bfd6610ee82d15766fbb98e0
edergilson <edergilson@gmail.com>
2020-09-16 23:56:36 -0300

    Update step3_kde.sh

--- arch/step3_kde.sh
+++ arch/step3_kde.sh
@@ -10,8 +10,8 @@ echo "
 [multilib]
 Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
 
-sudo pacman -Sy nvidia nvidia-utils mesa bumblebee nvidia-settings lib32-virtualgl lib32-nvidia-utils
-sudo usermod -aG bumblebee eder && sudo systemctl enable bumblebeed.service
+sudo pacman -Sy nvidia nvidia-utils mesa nvidia-prime nvidia-settings lib32-virtualgl lib32-nvidia-utils
+# sudo usermod -aG bumblebee eder && sudo systemctl enable bumblebeed.service
 
 sudo pacman -Sy plasma-meta sddm xorg xorg-xinit ttf-dejavu ttf-liberation libinput xf86-input-evdev networkmanager
 sudo pacman -Sy konsole okular ark kate gwenview dolphin p7zip unrar tar rsync linux-headers
