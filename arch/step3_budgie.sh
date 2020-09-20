473b02208cb402ebde91f8e58f9c93c28524105b
edergilson <edergilson@gmail.com>
2020-09-20 18:15:23 -0300

    Create step3_budgie.sh

---
+++ arch/step3_budgie.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#sudo ip link set $1 up
+#sudo dhcpcd $1
+
+echo "
+[multilib]
+Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
+
+sudo pacman -Sy nvidia nvidia-utils mesa nvidia_prime nvidia-settings lib32-virtualgl lib32-nvidia-utils
+# sudo usermod -aG bumblebee eder && sudo systemctl enable bumblebeed.service
+
+sudo pacman -Syu budgie_desktop ttf-dejavu ttf-hack ttf-liberation cantarell-fonts noto-fonts gdm xorg xorg-xinit libinput xf86-input-evdev networkmanager p7zip unrar tar rsync linux-headers
+sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
+sed -i '/exec /s/^/# /g' ~/.xinitrc
+echo "export XDG_CURRENT_DESKTOP=Budgie:GNOME
+exec budgie-desktop" >> ~/.xinitrc
+sudo systemctl enable NetworkManager
+sudo systemctl enable gdm
+
+sudo reboot
