31dae47151d626abb9c472fca70b7b8a258db05b
edergilson <edergilson@gmail.com>
2024-10-22 11:19:50 -0300

    Update install.sh

--- opensuse/install.sh
+++ opensuse/install.sh
@@ -37,9 +37,21 @@ sudo rpm --import https://dl.google.com/linux/linux_signing_key.pub
 sudo zypper in bzip2 cabextract lhasa lzip 7zip unrar unzip zip
 sudo zypper in fish vim clementine youtube-dl meld htop btop docker docker-compose lutris git google-chrome-stable brave-browser sublime-text code audacity kodi gimp inkscape kdenlive krita discord steam microsoft-edge-stable megasync dolphin-megasync opi
 
+# Other Modules
+## Sierra Breeze Theme
+sudo zypper in cmake extra-cmake-modules kf6-kiconthemes-devel qt6-qt5compat-devel kf6-kcmutils-devel kf6-kcoreaddons-devel kf6-kcoreaddons-devel libQt6Core6 libQt6Gui6 libQt6DBus6 libKF6GuiAddons6 libKF6WindowSystem6 libKF6I18n6 libKF6CoreAddons6 libKF6ConfigWidgets6 libkdecorations2-6
+git clone https://github.com/kupiqu/SierraBreezeEnhanced.git ~/Git/SierraBreezeEnhanced/ && cd ~/Git/SierraBreezeEnhanced/ && chmod +x install.sh && ./install.sh
+
+## Window Title and Buttons Applet
+sudo zypper in kf6-ksvg-devel kf6-kdeclarative-devel libplasma6-devel
+git clone https://github.com/moodyhunter/applet-window-buttons6.git ~/Git/applet-window-buttons6/ && cd ~/Git/applet-window-buttons6/ && ./install.sh
+sudo git clone https://github.com/dhruv8sh/plasma6-window-title-applet.git /usr/share/plasma/plasmoids/org.kde.windowtitle && sudo rm /usr/share/plasma/plasmoids/org.kde.windowtitle/README.md
+
+
 # Post Install Activities
 sudo usermod -aG docker $USER && sudo systemctl enable docker
 curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl && sudo chmod +x betterdiscordctl && sudo mv betterdiscordctl /usr/local/bin
 opi codecs
 chsh -s /usr/bin/fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
