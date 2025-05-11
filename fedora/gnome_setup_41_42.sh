643801aa33cc0109bb29d8344cb4f14e69826973
edergilson <edergilson@gmail.com>
2025-05-11 15:43:09 -0300

    Update and rename gnome_setup_41.sh to gnome_setup_41_42.sh

--- fedora/gnome_setup_41_42.sh
+++ fedora/gnome_setup_41_42.sh
@@ -16,12 +16,8 @@ sudo dnf group upgrade -y core
 # Media Codecs
 sudo dnf group upgrade -y 'core' 'multimedia' --setopt='install_weak_deps=False' --allowerasing && sync
 sudo dnf swap -y 'ffmpeg-free' 'ffmpeg' --allowerasing
-sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
-sudo dnf install -y lame\* --exclude=lame-devel
-sudo dnf group upgrade -y 'multimedia'
-sudo dnf install -y ffmpeg ffmpeg-libs libva libva-utils
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=true
-sudo dnf install -y openh264 gstreamer1-plugin-openh264 mozilla-openh264
+sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav lame\* ffmpeg ffmpeg-libs libva libva-utils openh264 gstreamer1-plugin-openh264 mozilla-openh264 --exclude=gstreamer1-plugins-bad-free-devel --exclude=lame-devel 
 
 # Flatpak Update
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && flatpak update
@@ -29,27 +25,25 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 # Importing Keys
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
 # Adding Repos
 printf "[vscode]\nname=packages.microsoft.com\nbaseurl=https://packages.microsoft.com/yumrepos/vscode/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscode.repo
 echo -e "[microsoft-edge]\nname=microsoft-edge\nbaseurl=https://packages.microsoft.com/yumrepos/edge/\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc"|sudo tee /etc/yum.repos.d/microsoft-edge.repo
 sudo dnf config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 sudo dnf copr enable -y observeroftime/betterdiscordctl
 
 # Enable packages
 sudo dnf config-manager setopt google-chrome.enabled=true
 
 # Apps
-sudo dnf install -y unzip p7zip p7zip-plugins unrar fish vim gnome-tweaks clementine youtube-dl audacity meld htop btop tilix docker docker-compose steam lutris git discord google-chrome-stable code microsoft-edge-stable sublime-text brave-browser lpf-spotify-client betterdiscordctl
-sudo dnf install -y mesa-dri-drivers.i686 mesa-libGL.i686 xorg-x11-drv-intel
+sudo dnf install -y mesa-dri-drivers.i686 mesa-libGL.i686 xorg-x11-drv-intel unzip p7zip p7zip-plugins unrar fish vim gnome-tweaks clementine youtube-dl audacity meld htop btop tilix docker docker-compose steam lutris git discord google-chrome-stable code microsoft-edge-stable sublime-text betterdiscordctl
 
+# Flatpaks
 flatpak install flathub io.github.lime3ds.Lime3DS net.kuribo64.melonDS net.pcsx2.PCSX2 com.snes9x.Snes9x io.mgba.mGBA org.ryujinx.Ryujinx nz.mega.MEGAsync
+
+# Configs
+sudo usermod -aG docker $USER && sudo systemctl enable docker
 
 # Install Oh-My-Fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 chsh -s /usr/bin/fish
-
-# Configs
-sudo usermod -aG docker $USER && sudo systemctl enable docker
