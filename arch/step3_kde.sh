3b0305b0c7d8a4c642d8c5501677a16d17dcb2c4
edergilson <edergilson@gmail.com>
2025-07-31 16:32:35 -0300

    Update step3_kde.sh

--- arch/step3_kde.sh
+++ arch/step3_kde.sh
@@ -2,19 +2,19 @@
 
 # sudo ip link set $1 up
 # sudo dhcpcd $1
-#sudo rfkill unblock wifi
-sudo wifi-menu
+# sudo rfkill unblock wifi
+# sudo wifi-menu
 
 ####### INSTALL KDE!!! #######
 echo "
 [multilib]
 Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
 
-sudo pacman -Sy nvidia nvidia-utils mesa nvidia-prime nvidia-settings lib32-virtualgl lib32-nvidia-utils
+sudo pacman -Sy nvidia nvidia-utils mesa nvidia-prime nvidia-settings lib32-virtualgl lib32-nvidia-utils lib32-mesa vulkan-intel lib32-vulkan-intel xf86-video-intel egl-wayland
 # sudo usermod -aG bumblebee eder && sudo systemctl enable bumblebeed.service
 
-sudo pacman -Sy plasma-meta sddm xorg xorg-xinit ttf-dejavu ttf-liberation libinput xf86-input-evdev networkmanager
-sudo pacman -Sy konsole okular ark kate gwenview dolphin p7zip unrar tar rsync linux-headers
+sudo pacman -Sy plasma-meta sddm ttf-dejavu ttf-liberation libinput xf86-input-evdev networkmanager
+sudo pacman -Sy konsole okular ark kate gwenview dolphin p7zip unrar tar rsync linux-headers print-manager cups system-config-printer powerdevil kscreen
 # sudo nano /etc/X11/xinit/xinitrc ###Comment all exec and add exec startkde
 sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
 sed -i '/exec /s/^/# /g' ~/.xinitrc
