b89a5e5d42cbb7895ee97ec2407447db36447ab5
edergilson <edergilson@gmail.com>
2025-07-31 15:16:05 -0300

    Update step1.sh

--- arch/step1.sh
+++ arch/step1.sh
@@ -12,6 +12,6 @@
 #cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bkp
 #echo "Server = http://archlinux.c3sl.ufpr.br/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 
-pacstrap /mnt base base-devel grub-efi-x86_64 efibootmgr intel-ucode os-prober git
-pacstrap /mnt bash bzip2 dialog wpa_supplicant cryptsetup device-mapper dhcpcd e2fsprogs findutils gawk gcc-libs gettext glibc inetutils iproute2 jfsutils licenses linux linux-firmware logrotate lvm2 man-db mdadm nano netctl pacman pciutils perl procps-ng psmisc reiserfsprogs s-nail shadow sysfsutils systemd-sysvcompat texinfo usbutils util-linux vi vim xfsprogs
+pacstrap /mnt base base-devel grub-efi-x86_64 efibootmgr intel-ucode os-prober git linux linux-firmware
+pacstrap /mnt bash bzip2 dialog wpa_supplicant cryptsetup device-mapper dhcpcd e2fsprogs findutils gawk gcc-libs gettext glibc inetutils iproute2 jfsutils licenses logrotate lvm2 man-db mdadm nano netctl pacman pciutils perl procps-ng psmisc reiserfsprogs s-nail shadow sysfsutils systemd-sysvcompat texinfo usbutils util-linux vi vim xfsprogs
 genfstab -U /mnt >> /mnt/etc/fstab
