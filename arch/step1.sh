cab0ff8cc251cab87fb440172229d4e8dc1e08b7
edergilson <edergilson@gmail.com>
2026-02-18 16:15:44 -0300

    Add NetworkManager to pacstrap installation

--- arch/step1.sh
+++ arch/step1.sh
@@ -13,5 +13,5 @@
 #echo "Server = http://archlinux.c3sl.ufpr.br/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 
 pacstrap /mnt base base-devel grub-efi-x86_64 efibootmgr intel-ucode os-prober git linux linux-firmware
-pacstrap /mnt bash bzip2 dialog wpa_supplicant cryptsetup device-mapper dhcpcd e2fsprogs findutils gawk gcc-libs gettext glibc inetutils iproute2 jfsutils licenses logrotate lvm2 man-db mdadm nano netctl pacman pciutils perl procps-ng psmisc s-nail shadow sysfsutils systemd-sysvcompat texinfo usbutils util-linux vi vim xfsprogs
+pacstrap /mnt bash bzip2 dialog wpa_supplicant cryptsetup device-mapper dhcpcd e2fsprogs findutils gawk gcc-libs gettext glibc inetutils iproute2 jfsutils licenses logrotate lvm2 man-db mdadm nano netctl pacman pciutils perl procps-ng psmisc s-nail shadow sysfsutils systemd-sysvcompat texinfo usbutils util-linux vi vim xfsprogs networkmanager
 genfstab -U /mnt >> /mnt/etc/fstab
