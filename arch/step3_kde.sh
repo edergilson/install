46af72360ee79417a9edc12450cc7c87c900b166
edergilson <edergilson@gmail.com>
2026-02-27 10:33:30 -0300

    Modify KDE setup script for package and service changes
    Updated package installation and enabled plasmalogin service.

--- arch/step3_kde.sh
+++ arch/step3_kde.sh
@@ -13,11 +13,12 @@ Include = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf
 sudo pacman -Sy nvidia nvidia-utils mesa nvidia-prime nvidia-settings lib32-virtualgl lib32-nvidia-utils lib32-mesa vulkan-intel lib32-vulkan-intel xf86-video-intel egl-wayland
 # sudo usermod -aG bumblebee eder && sudo systemctl enable bumblebeed.service
 
-sudo pacman -Sy plasma-meta sddm ttf-dejavu ttf-liberation libinput xf86-input-evdev networkmanager
+sudo pacman -Sy plasma-meta plasma-login-manager ttf-dejavu ttf-liberation libinput xf86-input-evdev networkmanager
 sudo pacman -Sy konsole okular ark kate gwenview dolphin p7zip unrar tar rsync linux-headers print-manager cups system-config-printer powerdevil kscreen
 # sudo nano /etc/X11/xinit/xinitrc ###Comment all exec and add exec startkde
-sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
-sed -i '/exec /s/^/# /g' ~/.xinitrc
-echo "exec startkde" >> ~/.xinitrc
+# sudo cp /etc/X11/xinit/xinitrc ~/.xinitrc
+# sed -i '/exec /s/^/# /g' ~/.xinitrc
+# echo "exec startkde" >> ~/.xinitrc
 sudo systemctl enable NetworkManager
-sudo systemctl enable sddm
+# sudo systemctl enable sddm
+sudo systemctl enable plasmalogin
